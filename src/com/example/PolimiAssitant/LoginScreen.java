3eabbadb511abb8cf062b07eaae165f13476fde9
neocloud19 <neocloud19@yahoo.com>
2014-04-04 00:22:23 +0200

    ADD Dialog box for responsibility disclaimer acceptance, INSTEAD of the Toast message

--- src/com/example/PolimiAssitant/LoginScreen.java
+++ src/com/example/PolimiAssitant/LoginScreen.java
@@ -1,6 +1,9 @@
 package com.example.PolimiAssitant;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
@@ -15,8 +18,13 @@ import android.widget.Toast;
  */
 public class LoginScreen extends Activity {
 
+    //TODO: Remove this variables when the test is finish
+    //Declare the strings that will be carried out to the following intent
     public final static String USERNAME = "com.example.PolimiAssitant.USERNAME";
     public final static String PASSWORD = "com.example.PolimiAssitant.PASSWORD";
+
+    //Declare the context of the application in case of need
+    final Context context = this;
 
     public void onCreate(Bundle savedInstanceState) {
 
@@ -55,35 +63,90 @@ public class LoginScreen extends Activity {
     }
 
     /**
-     * Checks if the user agrees to store
-     * the username and password in the device, facing
-     * all the possible consequences and liberating the app
-     * from any responsibility.
-     *
+     * Checks if the user agrees to store the username and
+     * password in the device, facing all the possible
+     * consequences and liberating the app from any
+     * responsibility.
      */
     private void addListenerOnCheckStorage() {
 
 
         //1. Search for the checkbox
-        CheckBox checkstorage = (CheckBox) findViewById(R.id.loginscreen_checkstore);
+        final CheckBox check_box;
+        check_box = (CheckBox) findViewById(R.id.loginscreen_checkstore);
 
         //2. Create the listener
-        checkstorage.setOnClickListener(new View.OnClickListener() {
+        check_box.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                String message;
+                String dialog_message;
+                String dialog_title;
+                String dialog_yes;
+                String dialog_no;
 
-                //3. is the box checked?
-                if(((CheckBox)v).isChecked()){
 
-                    //3.1 Load the acceptance of responsibility message
-                    message = getResources().getString(R.string.login_store_responsibility);
-                    //3.2 Display the message
-                    Toast.makeText(getApplicationContext(),message, Toast.LENGTH_LONG).show();
+                //3. If the box is checked, perform the corresponding actions
+                if (((CheckBox) v).isChecked()) {
 
-                }else{
+                    //3.1 Load the acceptance of responsibility strings
+
+                    dialog_message = getResources().getString(R.string.
+                            login_storing_responsibility_message);
+                    dialog_title = getResources().getString(R.string.
+                            login_storing_responsibility_title);
+                    dialog_yes = getResources().getString(R.string.
+                            login_storing_responsibility_yes);
+                    dialog_no = getResources().getString(R.string.
+                            login_storing_responsibility_no);
+
+                    //3.1 Alert dialog initialization
+                    AlertDialog.Builder responsibilityMessage = new AlertDialog.Builder(context);
+
+                    //3.2 Set dialog title
+                    responsibilityMessage.setTitle(dialog_title);
+
+                    //3.3 Set dialog message
+                    responsibilityMessage.setMessage(dialog_message);
+
+                    //3.4 Declare dialog settings and actions
+
+                    //Non cancellable dialog
+                    responsibilityMessage.setCancelable(false);
+
+                    //If the user accept the responsability
+                    responsibilityMessage.setPositiveButton(dialog_yes,
+                            new DialogInterface.OnClickListener() {
+
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    //When clicked close the dialog
+                                    dialog.cancel();
+
+                                }
+                            });
+
+                    //If the user dont want to accept the responsability
+                    responsibilityMessage.setNegativeButton(dialog_no,
+                            new DialogInterface.OnClickListener() {
+
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    //When clicked, uncheck the checkbox and close the dialog
+                                    check_box.setChecked(false);
+                                    dialog.cancel();
+
+                                }
+                            });
+
+                    //4. Create and display the dialog
+                    AlertDialog responsibilityDialog = responsibilityMessage.create();
+                    responsibilityDialog.show();
+
+                } else {
                     //No message displayed
                 }
             }
@@ -116,12 +179,12 @@ public class LoginScreen extends Activity {
     }
 
     /**
-     * Login class that process the login information and
+     * Login class that process the login information, login settings and
      * calls the correct classes to handle this.
      *
      * @param view
      *
-     * TODO: Add the store key method
+     * TODO: Add the store key method and save the storing login details if needed
      */
     public void login(View view){
 
